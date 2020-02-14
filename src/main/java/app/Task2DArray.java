84e160b34b57382f2a869881449a2cb4b9ebd9b2
elgun.cumayev <elgun.cumayev@mail.ru>
2020-02-14 16:51:23 +0400

    Task 2D Array

---
+++ src/main/java/app/Task2DArray.java
@@ -0,0 +1,25 @@
+package app;
+
+import java.util.Scanner;
+
+public class Task2DArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int column = scan.nextInt();
+        int[][] Array2D = new int[row][column];
+
+        for (int y = 0; y < row; y++) {
+            for (int x = 0; x < column; x++) {
+                Array2D[y][x]= Math.min(Math.min(x, y)+1,Math.min(column-x,row-y));
+            }
+
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(Array2D[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
