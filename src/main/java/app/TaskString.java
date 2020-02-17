f77223c4abfcb75371fd10595032ea455a12c228
elgun.cumayev <elgun.cumayev@mail.ru>
2020-02-17 20:16:02 +0400

    Task String 2

--- src/main/java/app/TaskString.java
+++ src/main/java/app/TaskString.java
@@ -4,26 +4,31 @@ import java.util.Scanner;
 
 public class TaskString {
     public static void main(String[] args) {
+        final String VOW = "aioueyAIOUEY";
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter size of String");
         int sizeString = scanner.nextInt();
         String firstString = "";
+        int randINT=91;
         for (int i = 0; i < sizeString; i++) {
-            firstString += (char)(Math.random()*('z'-'a'+1)+'a');
+            randINT = (int)(Math.random()*('z'-'A'+1)+'A');
+            if(randINT>=91 && randINT<=96) i--;
+            else {
+                firstString+=(char)randINT;
+            }
         }
         String vowelString = "", consString = "";
+        //int[] letters = new int[26];
         for (int i = 0; i < sizeString; i++) {
-            char indexChar = firstString.charAt(i);
-            if(indexChar == 'a' ||
-            indexChar == 'e' ||
-            indexChar == 'i' ||
-            indexChar == 'o' ||
-            indexChar == 'u' ||
-            indexChar == 'y')  vowelString+=indexChar;
-            else consString +=indexChar;
+            char c = firstString.charAt(i);
+            if (VOW.contains(String.valueOf(c))) vowelString += c;
+            else consString += c;
+            //letters[(int) c - 97]++;
         }
-        System.out.printf("First string: %s\nVowels string: %s\nConstants string: %s\n",firstString,vowelString,consString);
-
+        System.out.printf("First string: %s\nVowels string: %s\nConsonants string: %s\n",firstString,vowelString,consString);
+        /*for (int i = 0; i < 26; i++) {
+            System.out.printf("%c   -   %d\n",(char)(i+97),letters[i]);
+        }*/
         
     }
 }
