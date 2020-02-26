985e26434ee52008c2040fb7c6d24df904b1fce8
elgun.cumayev <elgun.cumayev@mail.ru>
2020-02-26 16:33:07 +0400

    Filter(correction 2nd time)

--- src/main/java/TestUnitApp/TaskTestUnit.java
+++ src/main/java/TestUnitApp/TaskTestUnit.java
@@ -9,14 +9,13 @@ public class TaskTestUnit implements Filterable {
     if (origin == null) {
       return null;
     }
-    origin = origin.trim();
     origin = origin.toLowerCase();
 
     StringBuilder result = new StringBuilder();
     for (int i = 0; i < origin.length(); i++) {
       char c = origin.charAt(i);
       if (!(LOWVOW.contains(String.valueOf(c)) || UPPVOW.contains(String.valueOf(c)))) {
-        if (!(c == ' ' && result.charAt(result.length()-1) == ' ')) result.append(c);
+        result.append(c);
       }
     }
     return result.toString();
--- src/test/java/TestUnitApp/TaskTestUnitTest.java
+++ src/test/java/TestUnitApp/TaskTestUnitTest.java
@@ -48,8 +48,8 @@ class TaskTestUnitTest {
 
   @Test
   void filter5() {
-    String origin = "   ##$%^&AHCehsbaucbihja  Wb ii dHodg  yGY AGHabd h Ee  II hyHEAHBb  WAH ibIdaHBADA Ydywhb ^@AaIo&Yi*Q o ASvbxgavs x   ";
-    String expected = "##$%^&hchsbcbhj wb dhdg ygy ghbd h hyhhbb wh bdhbd ydywhb ^@&y*q svbxgvs x";
+    String origin = "   ##$0%^&AHCehsbaucbihja  Wb ii dHodg  yGY AGHabd h Ee  II hyHEAHBb  WAH ibIdaHBADA Ydywhb ^@AaIo&Yi*Q o ASvbxgavs x   ";
+    String expected = "   ##$0%^&hchsbcbhj  wb  dhdg  ygy ghbd h    hyhhbb  wh bdhbd ydywhb ^@&y*q  svbxgvs x   ";
     String actual = task.filter(origin);
     assertEquals(expected, actual);
   }
@@ -57,7 +57,15 @@ class TaskTestUnitTest {
   @Test
   void filter6() {
     String origin = "Sometimes to understand a word's meaning you need more than a definition.";
-    String expected = "smtms t ndrstnd wrd's mnng y nd mr thn dfntn.";
+    String expected = "smtms t ndrstnd  wrd's mnng y nd mr thn  dfntn.";
+    String actual = task.filter(origin);
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  void filter7() {
+    String origin = " ";
+    String expected = " ";
     String actual = task.filter(origin);
     assertEquals(expected, actual);
   }
