7ee7734e498b80a0773eef9b3d83630cec953e05
elgun.cumayev <elgun.cumayev@mail.ru>
2020-02-26 16:57:08 +0400

    Filter(correction 3rd time)

--- src/main/java/TestUnitApp/TaskTestUnit.java
+++ src/main/java/TestUnitApp/TaskTestUnit.java
@@ -2,19 +2,19 @@ package TestUnitApp;
 
 public class TaskTestUnit implements Filterable {
   final String LOWVOW = "aioue";
-  final String UPPVOW = "AIOUE";
 
   @Override
   public String filter(String origin) {
     if (origin == null) {
       return null;
     }
+
     origin = origin.toLowerCase();
 
     StringBuilder result = new StringBuilder();
     for (int i = 0; i < origin.length(); i++) {
       char c = origin.charAt(i);
-      if (!(LOWVOW.contains(String.valueOf(c)) || UPPVOW.contains(String.valueOf(c)))) {
+      if (!LOWVOW.contains(String.valueOf(c))) {
         result.append(c);
       }
     }
