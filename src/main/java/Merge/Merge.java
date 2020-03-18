0a067fee562b9221d9e1a10e47a787488d511854
elgun.cumayev <elgun.cumayev@mail.ru>
2020-03-18 17:55:52 +0400

    Merge

---
+++ src/main/java/Merge/Merge.java
@@ -0,0 +1,34 @@
+package Merge;
+
+import java.awt.image.AreaAveragingScaleFilter;
+import java.util.Arrays;
+import java.util.Random;
+
+public class Merge {
+  public static void main(String[] args) {
+    int[] data1 = new Random().ints(10, 30).limit(20).sorted().toArray();
+    int[] data2 = new Random().ints(10, 30).limit(20).sorted().toArray();
+    int[] data3 = merge(data1, data2);
+    System.out.println(Arrays.toString(data1));
+    System.out.println(Arrays.toString(data2));
+    System.out.println(Arrays.toString(data3));
+  }
+
+  private static int[] merge(int[] data1, int[] data2) {
+    int size1 = data1.length;
+    int size2 = data2.length;
+    int in1=0,in2=0;
+    int[] result = new int[size1+size2];
+    for (int i = 0; i < size1 + size2 -1; i++) {
+      if(data1[in1] > data2[in2]){
+        result[i] = data2[in2];
+        in2++;
+      }
+      else {
+        result[i] = data1[in1];
+        in1++;
+      }
+    }
+    return result;
+  }
+}
